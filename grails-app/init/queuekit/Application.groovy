e4c2345e32a1326b44e5e65889e555cd4e0d0b99
vahidhedayati <badvad@gmail.com>
2016-10-15 23:12:21 +0100

    tidy up

--- grails-app/init/queuekit/Application.groovy
+++ grails-app/init/queuekit/Application.groovy
@@ -4,7 +4,6 @@ import grails.boot.*
 import grails.boot.config.GrailsAutoConfiguration
 import grails.plugins.metadata.*
 
-@PluginSource
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
         GrailsApp.run(Application, args)
